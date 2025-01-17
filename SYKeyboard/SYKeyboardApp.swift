41c77e30f40e7da914374ba6f212f49d3d2cbb7b
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2025-01-17 11:37:52 +0900

    [feat] Google Analytics용 Firebase 추가

--- SYKeyboard/SYKeyboardApp.swift
+++ SYKeyboard/SYKeyboardApp.swift
@@ -6,9 +6,25 @@
 //
 
 import SwiftUI
+import GoogleMobileAds
+import Firebase
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        FirebaseApp.configure()
+        GADMobileAds.sharedInstance().start(completionHandler: nil)
+        
+        return true
+    }
+}
 
 @main
 struct SYKeyboardApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
