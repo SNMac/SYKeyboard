bd733c9c56c36920ec35017fc0f8fc2ef3d37f31
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2025-01-17 11:46:34 +0900

    [archive] 202501172

--- SYKeyboard/SYKeyboardApp.swift
+++ SYKeyboard/SYKeyboardApp.swift
@@ -14,7 +14,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
+        // Use Firebase library to configure APIs.
         FirebaseApp.configure()
+        
+        // Initialize the Google Mobile Ads SDK.
         GADMobileAds.sharedInstance().start(completionHandler: nil)
         
         return true
