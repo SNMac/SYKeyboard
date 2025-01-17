19175e884613c29e8c7f55e316e274be2f8ec3af
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2025-01-17 15:57:55 +0900

    [feat] AdMob용 추적 권한 요청 코드 추가

--- SYKeyboard/SYKeyboardApp.swift
+++ SYKeyboard/SYKeyboardApp.swift
@@ -6,13 +6,37 @@
 //
 
 import SwiftUI
+import AdSupport
+import AppTrackingTransparency
+import OSLog
 import GoogleMobileAds
 import Firebase
 
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
-    func application(_ application: UIApplication,
-                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let log = OSLog(subsystem: "github.com-SNMac.SYKeyboard", category: "AppDelegate")
+        
+        // 앱 추적 권한 요청
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            if #available(iOS 14, *) {
+                ATTrackingManager.requestTrackingAuthorization { status in
+                    switch status {
+                    case .authorized:  // 허용됨
+                        let idfa: String = "\(ASIdentifierManager.shared().advertisingIdentifier)"
+                        os_log("Tracking Authorized, IDFA = %{private}@", log: log, type: .debug, idfa)
+                    case .denied:  // 거부됨
+                        os_log("Tracking Denied", log: log, type: .debug)
+                    case .notDetermined:  // 결정되지 않음
+                        os_log("Tracking Not Determined", log: log, type: .debug)
+                    case .restricted:  // 제한됨
+                        os_log("Tracking Restricted", log: log, type: .debug)
+                    @unknown default:  // 알려지지 않음
+                        os_log("Tracking Unknow", log: log, type: .debug)
+                    }
+                }
+            }
+        }
         
         // Use Firebase library to configure APIs.
         FirebaseApp.configure()
