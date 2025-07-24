23d0b7691d93cd419f660a9b1554b10a625bae7b
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2025-07-24 16:31:01 +0900

    feat: #6 - 다크모드 컬러 에셋 추가

---
+++ Keyboard/Presentation/View/Components/Overlays/KeyboardLayoutSelectOverlayView.swift
@@ -0,0 +1,12 @@
+//
+//  KeyboardLayoutSelectOverlayView.swift
+//  Keyboard
+//
+//  Created by 서동환 on 7/24/25.
+//
+
+import UIKit
+
+final class KeyboardLayoutSelectOverlayView: UIView {
+    
+}
