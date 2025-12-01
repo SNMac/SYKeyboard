ad9197bb887bb4ed2847e5918bbb040626fc2d0d
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2025-12-02 03:36:29 +0900

    fix: #24 - 누락된 Localizable String 반영

--- SYKeyboard/Presentation/KeyboardSettings/InputSettingsView.swift
+++ SYKeyboard/Presentation/KeyboardSettings/InputSettingsView.swift
@@ -31,12 +31,21 @@ struct InputSettingsView: View {
     @AppStorage(UserDefaultsKeys.isDragToMoveCursorEnabled, store: UserDefaults(suiteName: DefaultValues.groupBundleID))
     private var isDragToMoveCursorEnabled = DefaultValues.isDragToMoveCursorEnabled
     
-    enum LongPressMode: String, CaseIterable, Identifiable {
-        case repeatInput = "반복 입력"
-        case numberInput = "숫자 입력"
-        case disabled = "비활성화"
+    enum LongPressMode: Int, CaseIterable {
+        case repeatInput
+        case numberInput
+        case disabled
         
-        var id: String { self.rawValue }
+        var displayStr: String {
+            switch self {
+            case .repeatInput:
+                String(localized: "반복 입력")
+            case .numberInput:
+                String(localized: "숫자 입력")
+            case .disabled:
+                String(localized: "비활성화")
+            }
+        }
     }
     
     private var longPressModeBinding: Binding<LongPressMode> {
@@ -68,8 +77,8 @@ struct InputSettingsView: View {
     
     var body: some View {
         Picker("길게 누르기 동작", selection: longPressModeBinding) {
-            ForEach(LongPressMode.allCases) { mode in
-                Text(mode.rawValue).tag(mode)
+            ForEach(LongPressMode.allCases, id: \.self) {
+                Text($0.displayStr)
             }
         }
         
