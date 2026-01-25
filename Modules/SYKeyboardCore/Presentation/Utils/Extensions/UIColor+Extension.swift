6d890967bbf7abaee2ef96a757c6817e4dee1671
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2026-01-26 07:09:31 +0900

    fix: #37 - 색상 강제 언래핑 수정

--- Modules/SYKeyboardCore/Presentation/Utils/Extensions/UIColor+Extension.swift
+++ Modules/SYKeyboardCore/Presentation/Utils/Extensions/UIColor+Extension.swift
@@ -13,42 +13,42 @@ public extension UIColor {
     
     /// PrimaryButtonColor
     static var primaryButton: UIColor {
-        return UIColor(named: "PrimaryButtonColor", in: .syKeyboardCoreResources, compatibleWith: nil)!
+        return UIColor(named: "PrimaryButtonColor", in: .syKeyboardCoreResources, compatibleWith: nil) ?? .systemBackground
     }
     
     /// PrimaryButtonPressedColor
     static var primaryButtonPressed: UIColor {
-        return UIColor(named: "PrimaryButtonPressedColor", in: .syKeyboardCoreResources, compatibleWith: nil)!
+        return UIColor(named: "PrimaryButtonPressedColor", in: .syKeyboardCoreResources, compatibleWith: nil) ?? .systemGroupedBackground
     }
     
     // MARK: - Secondary Button
     
     /// SecondaryButtonColor
     static var secondaryButton: UIColor {
-        return UIColor(named: "SecondaryButtonColor", in: .syKeyboardCoreResources, compatibleWith: nil)!
+        return UIColor(named: "SecondaryButtonColor", in: .syKeyboardCoreResources, compatibleWith: nil) ?? .secondarySystemBackground
     }
     
     /// SecondaryButtonPressedColor
     static var secondaryButtonPressed: UIColor {
-        return UIColor(named: "SecondaryButtonPressedColor", in: .syKeyboardCoreResources, compatibleWith: nil)!
+        return UIColor(named: "SecondaryButtonPressedColor", in: .syKeyboardCoreResources, compatibleWith: nil) ?? .secondarySystemGroupedBackground
     }
     
     // MARK: - Chevron Button
     
     /// ChevronButtonColor
     static var chevronButton: UIColor {
-        return UIColor(named: "ChevronButtonColor", in: .syKeyboardCoreResources, compatibleWith: nil)!
+        return UIColor(named: "ChevronButtonColor", in: .syKeyboardCoreResources, compatibleWith: nil) ?? .systemBackground
     }
     
     /// ChevronButtonPressedColor
     static var chevronButtonPressed: UIColor {
-        return UIColor(named: "ChevronButtonPressedColor", in: .syKeyboardCoreResources, compatibleWith: nil)!
+        return UIColor(named: "ChevronButtonPressedColor", in: .syKeyboardCoreResources, compatibleWith: nil) ?? .systemGroupedBackground
     }
     
     // MARK: - Shadow
     
     /// ButtonShadowColor
     static var buttonShadow: UIColor {
-        return UIColor(named: "ButtonShadowColor", in: .syKeyboardCoreResources, compatibleWith: nil)!
+        return UIColor(named: "ButtonShadowColor", in: .syKeyboardCoreResources, compatibleWith: nil) ?? .darkGray
     }
 }
