ff47ed30a8525aacc0ad92d788f7c6e27bead1dc
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2026-03-19 17:19:20 +0900

    feat: #31 - 활성화 드래그 거리 값 조정

--- Modules/SYKeyboardCore/Storage/DefaultValues.swift
+++ Modules/SYKeyboardCore/Storage/DefaultValues.swift
@@ -46,7 +46,7 @@ public enum DefaultValues {
     /// 키 반복 속도 기본값
     public static let repeatRate: Double = 0.05
     /// 활성화 드래그 거리 기본값
-    public static let cursorActiveDistance: Double = 50.0
+    public static let cursorActiveDistance: Double = 40.0
     /// 이동 드래그 간격 기본값
     public static let cursorMoveInterval: Double = 5.0
     
